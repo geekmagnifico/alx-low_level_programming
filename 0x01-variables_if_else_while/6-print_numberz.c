4927127cfc071f51ddbd9302468d50528e60e92f
geekmagnifico <burmani.nabi@gmail.com>
2023-08-11 18:42:01 -0700

    fixing

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -5,14 +5,13 @@
  * starting from 0, followed by a new line.
  * Return: 0
  */
-int main(void) {
-    int i;
+int main(void)
+{
+	int i;
 
-    for (i = 0; i <= 9; i++) {
-        putchar('0' + i);
-    }
+	for (i = 0; i <= 9; i++)
+	putchar('0' + i);
 
-    putchar('\n');
-
-    return 0;
+	putchar('\n');
+	return (0);
 }
