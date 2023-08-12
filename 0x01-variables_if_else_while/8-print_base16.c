84b009e72625eb37a2a01b3b475622960ab9d9f4
geekmagnifico <burmani.nabi@gmail.com>
2023-08-11 18:52:45 -0700

    fixing

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -2,23 +2,20 @@
 
 /**
  * main - main block
- * Description: Print all the numbers of base 16 in lowercase, followed by a new line.
+ * Description: Print all the numbers of base 16
+ * in lowercase, followed by a new line.
  * Return: 0
  */
-int main(void) {
-    char hex_digit;
+int main(void)
+{
+	char hex_digit;
 
-    // Print numbers 0-9
-    for (hex_digit = '0'; hex_digit <= '9'; hex_digit++) {
-        putchar(hex_digit);
-    }
+	for (hex_digit = '0'; hex_digit <= '9'; hex_digit++)
+		putchar(hex_digit);
 
-    // Print letters a-f
-    for (hex_digit = 'a'; hex_digit <= 'f'; hex_digit++) {
-        putchar(hex_digit);
-    }
+	for (hex_digit = 'a'; hex_digit <= 'f'; hex_digit++)
+		putchar(hex_digit);
 
-    putchar('\n');
-
-    return 0;
+	putchar('\n');
+	return (0);
 }
