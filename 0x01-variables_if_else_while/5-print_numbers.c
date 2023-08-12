88be8b6c90da4c5c521905276230b7f20213c716
geekmagnifico <burmani.nabi@gmail.com>
2023-08-11 18:31:46 -0700

    fixing

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,19 +1,18 @@
 #include <stdio.h>
-
 /**
  * main - main block
  * Description: Print all single digit numbers of base 10 starting from 0, followed by a new line.
  * Return: 0
  */
-int main(void) {
-    char digit = '0';
-
-    while (digit <= '9') {
+int main(void)
+{
+        char digit = '0';
+        while (digit <= '9')
+        {
         putchar(digit);
         digit++;
-    }
+        }
 
-    putchar('\n');
-
-    return 0;
+        putchar('\n');
+	return (0);
 }
