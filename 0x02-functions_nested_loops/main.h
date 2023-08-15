abd4edb2fb696a97d63468fd4629aaaa6a2bd9f4
geekmagnifico <burmani.nabi@gmail.com>
2023-08-15 13:15:38 -0700

    commit for checking 3

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -8,10 +8,7 @@
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
+int _putchar(char c);
 
 /**
  * print_alphabet - print lowercase alphabet
