0b40f891c759eeda169d9cd8e962efbbbc224460
geekmagnifico <burmani.nabi@gmail.com>
2023-08-21 17:40:23 -0700

    1

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
- * swap_int - swape two pointers 
+ * swap_int - swape two pointers
  * @a: first pointer
  * @b: seconde pointer
- * 
+ *
  */
 
 void swap_int(int *a, int *b)
 {
 	int p;
-	
+
 	p = *a;
 	*a = *b;
 	*b = p;
