d8285d424c1e0311473d4ba72b6651464d1449ea
geekmagnifico <burmani.nabil@gmail.com>
2023-08-30 12:01:23 -0700

    advanced tasks 100 and 10(fixing some errs secondly)1

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,28 +1,36 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
- * string_length - obtains length of s
- * @s: string
- * @length: integer to count length
+ * palind2 - obtains length of a
+ * @a: string
+ * @l: integer to count length
  *
- * Return: On success the length
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int palind2(char *a, int l)
+{
+	if (*a == 0)
+		return (l - 1);
+	return (palind2(a + 1, l + 1));
+}
+/**
+ * palind3 - compares string vs string reverse
+ * @a: string
+ * @l: length
+ *
+ * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
 
-
-int string_length(char *s)
+int palind3(char *a, int l)
 {
-	int length = 0;
-	
-	while (s[length] != '\0')
-	{
-		length++;
-	}
-
-	return length;
+	if (*a != *(a + l))
+		return (0);
+	else if (*a == 0)
+		return (1);
+	return (palind3(a + 1, l - 2));
 }
-
 /**
  * is_palindrome - checks if a string is a palindrome
  * @s: string to evaluate
@@ -30,20 +38,10 @@ int string_length(char *s)
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-
 int is_palindrome(char *s)
 {
-	int i, j, length = string_length(s);
+	int l;
 
-	length = string_length(s);
-
-	for (i = 0, j = length - 1; i < j; i++, j--)
-	{
-		if (s[i] != s[j]) 
-		{
-			return 0; /* Not a palindrome*/
-		}
-	}
-
-	return 1; /* Palindrome*/
+	l = palind2(s, 0);
+	return (palind3(s, l));
 }
