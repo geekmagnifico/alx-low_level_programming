b3bfd066aa73f47fd223ba6638d09542fb389829
geekmagnifico <burmani.nabil@gmail.com>
2023-09-03 12:02:32 -0700

    olala

--- 0x09-static_libraries/0-putchar.c
+++ 0x09-static_libraries/0-putchar.c
@@ -1,12 +1,12 @@
-#include <stdio.h>
+#include <unistd.h>
 
 /**
- * main - print _putchar, followed by new line
- * Return: 0
+ * _putchar - Writes a character to the standard output (stdout)
+ * @c: The character to be written
+ *
+ * Return: On success, 1. On error, -1 is returned, and errno is set
+ * appropriately.
  */
-
-int main(void)
-{
-	printf("_putchar\n");
-	return (0);
+int _putchar(char c) {
+    return write(1, &c, 1);
 }
