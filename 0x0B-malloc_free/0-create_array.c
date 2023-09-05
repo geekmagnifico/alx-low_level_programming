19a8794a72bc19268d79ffc2341bc56f21a7ff30
geekmagnifico <burmani.nabil@gmail.com>
2023-09-04 18:23:32 -0700

    eff

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,6 +10,7 @@
 char *create_array(unsigned int size, char c)
 {
     char *array;
+    unsigned int i;
 
     if (size == 0)
         return (NULL);
@@ -19,7 +20,7 @@ char *create_array(unsigned int size, char c)
     if (array == NULL)
         return (NULL);
 
-    for (unsigned int i = 0; i < size; i++)
+    for (i = 0; i < size; i++)
     {
         array[i] = c;
     }
