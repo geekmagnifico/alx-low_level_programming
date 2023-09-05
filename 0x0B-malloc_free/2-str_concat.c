2b88382f4c6a68955cb42f64536ece96a8e2bdbf
geekmagnifico <burmani.nabil@gmail.com>
2023-09-04 18:37:39 -0700

    reglage

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,38 +1,41 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
 
 /**
- * string_nconcat - function that concatenates two strings
- * @s1: string one being passed
- * @s2: string two being passed
- * @n: size
- * Return: Always 0.
+ * str_concat - concatenates two strings.
+ * @s1: first string.
+ * @s2: second string.
+ * Return: pointer of an array of chars
  */
-char *string_nconcat(char *s1, char *s2, unsigned int n)
+char *str_concat(char *s1, char *s2)
 {
-	char *p;
-	unsigned int a, b, c, d;
+char *strout;
+unsigned int i, j, k, limit;
 
-	if (s1 == 0)
-		s1 = "";
-	if (s2 == 0)
-		s2 = "";
-	for (a = 0; s1[a] != '\0'; a++)
-		;
-	for (b = 0; s2[b] != '\0'; b++)
-		;
-	if (n > b)
-		p = malloc((a + b + 1) * sizeof(char));
-	else
-		p = malloc((a + n + 1) * sizeof(char));
-	if (p == 0)
-		return (NULL);
+if (s1 == NULL)
+s1 = "";
 
-	for (c = 0; c < a; c++)
-		p[c] = s1[c];
-	for (d = 0; d < n && d < b; d++, c++)
-		p[c] = s2[d];
-	p[c] = '\0';
-	return (p);
+if (s2 == NULL)
+s2 = "";
+
+for (i = 0; s1[i] != '\0'; i++)
+;
+for (j = 0; s2[j] != '\0'; j++)
+;
+strout = malloc(sizeof(char) * (i + j + 1));
+
+if (strout == NULL)
+{
+free(strout);
+return (NULL);
+}
+
+for (k = 0; k < i; k++)
+strout[k] = s1[k];
+
+limit = j;
+for (j = 0; j <= limit; k++, j++)
+strout[k] = s2[j];
+
+return (strout);
 }
