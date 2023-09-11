4ba7644fe7f086caa571724289e343746030f2e2
geekmagnifico <burmani.nabil@gmail.com>
2023-09-11 15:55:04 -0700

    all tasks

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,11 @@
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
+
+/*
+ * Desc: header defines a macro
+ *       SIZE as an abbreviation for the token 1024.
+ */
+
+#define SIZE 1024
+
+#endif
